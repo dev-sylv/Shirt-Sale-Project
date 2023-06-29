a8e9e7d8532afc63ea4ed920956b9fafa550a611
nicsylvia <nicsylvia15f@gmail.com>
2023-06-29 09:51:58 -0700

    saved changes

--- src/Components/blocks/Homeblocks/Header.tsx
+++ src/Components/blocks/Homeblocks/Header.tsx
@@ -1,25 +1,46 @@
 import React from "react";
 import { GlobalButton } from "../../commons";
+import styled from "styled-components";
 
 const Header = () => {
-  return <div>
-    <Container>
-      <Wrapper>
-        <Logo>Sylv-Shirts</Logo>
-        <Navigations>
-          <Navs>Home</Navs>
-          <Navs>Products</Navs>
-          <Navs>About us</Navs>
-          <Navs>Features</Navs>
-        </Navigations>
-        <Buttons>
-          <GlobalButton bgC="" borderR="" text="" CC="" />
-        </Buttons>
-      </Wrapper>
-    </Container>
-  </div>;
+  return (
+    <div>
+      <Container>
+        <Wrapper>
+          <Logo>Sylv-Shirts</Logo>
+          <Navigations>
+            <Navs>Home</Navs>
+            <Navs>Products</Navs>
+            <Navs>About us</Navs>
+            <Navs>Features</Navs>
+          </Navigations>
+          <Buttons>
+            <GlobalButton bgC="" borderR="" text="" CC="" />
+          </Buttons>
+        </Wrapper>
+      </Container>
+    </div>
+  );
 };
 
 export default Header;
 
-const Container = 
+const Container = styled.div`
+  width: 100%;
+  height: 70px;
+  background-color: transparent;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+  width: 90%;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Logo = styled.div``;
+const Navigations = styled.div``;
+const Navs = styled.div``;
+const Buttons = styled.div``;
