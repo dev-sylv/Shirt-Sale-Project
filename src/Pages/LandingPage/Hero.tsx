8db8ae4840341df6690f4e5ed3f60fd05dcddab8
nicsylvia <nicsylvia15f@gmail.com>
2023-06-30 10:11:11 -0700

    working on hero

--- src/Pages/LandingPage/Hero.tsx
+++ src/Pages/LandingPage/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { GlobalButton } from "../../Components/commons";
+import { styled } from "styled-components";
 
 const Hero = () => {
   return (
@@ -12,14 +13,20 @@ const Hero = () => {
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro
               ratione vero quis, ad fugiat totam nulla inventore cum
             </MiniText>
-            <Button>
-              <GlobalButton
-                bgC="#800080ac"
-                borderR="8px"
-                text="Start for Free"
-                CC="white"
-              />
-            </Button>
+            <Div>
+              <Button>
+                <GlobalButton
+                  bgC="#800080ac"
+                  borderR="8px"
+                  text="Start for Free"
+                  CC="white"
+                />
+              </Button>
+              <Video>
+                <Icon></Icon>
+                <p>Watch Video</p>
+              </Video>
+            </Div>
           </TextContent>
           <ImageContent></ImageContent>
         </Wrapper>
@@ -29,3 +36,19 @@ const Hero = () => {
 };
 
 export default Hero;
+
+const Container = styled.div`
+  width: 100%;
+  height: 70px;
+  background-color: transparent;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+  width: 90%;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
