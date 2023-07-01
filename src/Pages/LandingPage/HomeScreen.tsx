7efa3c4f5cbde7fb20507ba409d0758fe9476788
nicsylvia <nicsylvia15f@gmail.com>
2023-07-01 12:23:31 -0700

    updates changed

--- src/Pages/LandingPage/HomeScreen.tsx
+++ src/Pages/LandingPage/HomeScreen.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Hero";
 import ShirtGallery from "./ShirtGallery";
+import Features from "./Features";
 
 const HomeScreen = () => {
   return (
     <div>
       <Hero />
       <ShirtGallery />
+      <Features />
     </div>
   );
 };
